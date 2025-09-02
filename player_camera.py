caed7d106fcb94c0e39291ba1151f7b10d9395e8
DCritt <dcritte@clemson.edu>
2025-09-02 14:20:09 -0400

    feature branch init

--- player_camera.py
+++ player_camera.py
@@ -11,25 +11,58 @@ class PlayerCamera:
 
         curr_ray_angle = self.player.player_angle - (FOV / 2) + 0.0001
         for ray in range(RAY_AMT):
-            ray_depth = RayCasting.cast_ray(self.player.pos, self.player.map_pos, curr_ray_angle, MAX_DEPTH, self.game.map.map_dic)
+            ray_info = RayCasting.cast_ray(self.player.pos, self.player.map_pos, curr_ray_angle, MAX_DEPTH, self.game.map.map_dic)
 
-            color = [255 * (1 - ray_depth / MAX_DEPTH)] * 3
+            if ray_info[0]:
 
-            ray_depth *= math.cos(self.player.player_angle - curr_ray_angle)
-            projection_height = SCREEN_DISTANCE / ray_depth
+                corrected_ray_depth = ray_info[1] * math.cos(self.player.player_angle - curr_ray_angle)
+                projection_height = SCREEN_DISTANCE / corrected_ray_depth
 
-            pygame.draw.rect(
-                self.game.screen,
-                color,
-                (ray * RAY_WIDTH_SCALE, (HEIGHT - projection_height) // 2, RAY_WIDTH_SCALE, projection_height)
+                projection_height = min(projection_height, HEIGHT)
+
+                darkness_multiplier = 0.7
+
+                distance_multiplier = (1 - ray_info[1] / MAX_DEPTH)
+
+                projection_height_segments = projection_height / 4
+
+                color = (0, 0, 0)
+
+                center = (WIDTH // 2, HEIGHT //2)
+
+                for segment in range(int(projection_height_segments)):
+
+                    # dist_from_center_screen_hor = abs((ray * RAY_WIDTH_SCALE) - (WIDTH // 2))
+                    # flash_light_multiplier_hor = 1 + (3 * (1 - (dist_from_center_screen_hor / (WIDTH // 2))))
+
+                    # dist_from_center_screen_vert = abs((((HEIGHT - projection_height) // 2) + (segment * 8)) - (HEIGHT // 2))
+                    # flash_light_multiplier_vert = 1 + (3 * (1 - (dist_from_center_screen_vert / (HEIGHT // 2))))
+
+                    location = (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + segment * 4)
+                    distance = math.dist(center, location)
+
+                    flashlight_multiplier = (max(((1 / darkness_multiplier) * ((1 / max(distance_multiplier, 0.4)))) * (max(0, 1 - (distance / 450))), darkness_multiplier))
+
+                    color = [255 * (darkness_multiplier * flashlight_multiplier * distance_multiplier)] * 3
+
+                    pygame.draw.rect(
+                        self.game.screen,
+                        color,
+                        (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + segment * 4, RAY_WIDTH_SCALE, 4)
+                    )
+                    
+                pygame.draw.rect(
+                    self.game.screen,
+                    color,
+                    (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + (4 * int(projection_height_segments)), RAY_WIDTH_SCALE,  ((projection_height_segments) - int(projection_height_segments)) * 4)
                 )
 
-            #pygame.draw.line(
-            #    self.game.screen, 
-            #    'yellow', 
-            #    (100 * self.player.pos[0], 100 * self.player.pos[1]),
-            #    (100 * self.player.pos[0] + 100 * ray_depth * math.cos(curr_ray_angle), 100 * self.player.pos[1] + 100 * ray_depth * math.sin(curr_ray_angle)),
-            #    2
-            #    )
+                #pygame.draw.line(
+                #    self.game.screen, 
+                #    'yellow', 
+                #    (100 * self.player.pos[0], 100 * self.player.pos[1]),
+                #    (100 * self.player.pos[0] + 100 * ray_depth * math.cos(curr_ray_angle), 100 * self.player.pos[1] + 100 * ray_depth * math.sin(curr_ray_angle)),
+                #    2
+                #    )
 
             curr_ray_angle += RAY_DELTA_ANGLE
--- settings.py
+++ settings.py
@@ -2,14 +2,12 @@ import math
 
 #screen settings
 RESOLUTION = WIDTH, HEIGHT = 1200, 700
-FPS = 60
-
-#raycasting settings
-FOV = math.pi / 3
-RAY_AMT = WIDTH // 2
-RAY_DELTA_ANGLE = FOV / RAY_AMT
-MAX_DEPTH = 20
+FPS = 120
 
 #3D projection settings
-SCREEN_DISTANCE = (WIDTH / 2) / math.tan(FOV / 2)
+FOV = math.pi / 3
+RAY_AMT = WIDTH // 4
+RAY_DELTA_ANGLE = FOV / RAY_AMT
+MAX_DEPTH = 15
 RAY_WIDTH_SCALE = WIDTH / RAY_AMT
+SCREEN_DISTANCE = (WIDTH / 2) / math.tan(FOV / 2)
