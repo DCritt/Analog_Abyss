4d2cbf0d7d51648f4cc1519cb9fa6aee2bd4c16d
DCritt <dcritte@clemson.edu>
2025-09-03 16:26:27 -0400

    feat: improved lighting code

--- lighting.py
+++ lighting.py
@@ -1,21 +1,11 @@
-from settings import DARKNESS_MULTIPLIER, MAX_LIGHT_DISTANCE, INVERSE_MAX_LIGHT_DISTANCE, SCREEN_CENTER, MAX_FLASHLIGHT_SCREEN_DISTANCE, INVERSE_MAX_FLASHLIGHT_SCREEN_DISTANCE, INVERSE_DARKNESS_MULTIPLIER, INVERSE_FLASHLIGHT_DISTANCE_NEGATION
+from settings import DARKNESS_MULTIPLIER, INVERSE_MAX_FLASHLIGHT_SCREEN_DISTANCE, INVERSE_DARKNESS_MULTIPLIER, INVERSE_FLASHLIGHT_DISTANCE_NEGATION
 
 class Lighting:
-
-    def calculate_lighting_multiplier(color, distance):
-        distance_multiplier = 0 if distance > MAX_LIGHT_DISTANCE else 1 - (distance * INVERSE_MAX_LIGHT_DISTANCE)
-
-        color_new = [color[1] * (DARKNESS_MULTIPLIER * distance_multiplier)]*3
-
-        #print(color_new, DARKNESS_MULTIPLIER, distance_multiplier)
-
-        return color_new
     
-    def calculate_lighting_multiplier_flashlight(color, distance, screen_location):
-        distance_multiplier = 0 if distance > MAX_LIGHT_DISTANCE else 1 - (distance * INVERSE_MAX_LIGHT_DISTANCE)
-        inverse_distance_multiplier = 1 / (distance_multiplier + 0.00001)
-
-        center_screen_distance = (SCREEN_CENTER[0] - screen_location[0])**2 + (SCREEN_CENTER[1] - screen_location[1])**2
-        flashlight_multiplier = 1 if center_screen_distance > MAX_FLASHLIGHT_SCREEN_DISTANCE else ((INVERSE_DARKNESS_MULTIPLIER * (inverse_distance_multiplier if inverse_distance_multiplier < INVERSE_FLASHLIGHT_DISTANCE_NEGATION else INVERSE_FLASHLIGHT_DISTANCE_NEGATION)) * (max(1 - (center_screen_distance * INVERSE_MAX_FLASHLIGHT_SCREEN_DISTANCE), DARKNESS_MULTIPLIER)))
+    def calculate_flashlight_multiplier(center_screen_distance, inverse_distance_multiplier):
+        distance_negation = inverse_distance_multiplier if inverse_distance_multiplier < INVERSE_FLASHLIGHT_DISTANCE_NEGATION else INVERSE_FLASHLIGHT_DISTANCE_NEGATION
         
-        return [color[1] * (distance_multiplier * flashlight_multiplier * DARKNESS_MULTIPLIER)]*3
+        screen_distance_proportion = 1 - (center_screen_distance * INVERSE_MAX_FLASHLIGHT_SCREEN_DISTANCE)
+        distance_from_center_multiplier = DARKNESS_MULTIPLIER if (screen_distance_proportion < DARKNESS_MULTIPLIER) else screen_distance_proportion
+
+        return ((INVERSE_DARKNESS_MULTIPLIER * distance_negation) * distance_from_center_multiplier)
--- player_camera.py
+++ player_camera.py
@@ -10,44 +10,63 @@ class PlayerCamera:
 
     def ray_cast(self):
 
+        darkness_multiplier = DARKNESS_MULTIPLIER
+        height = HEIGHT
+        ray_width_scale = RAY_WIDTH_SCALE
+        light_seg_size = LIGHT_SEG_SIZE
+        ray_delta_angle = RAY_DELTA_ANGLE
+        screen_distance = SCREEN_DISTANCE
+        max_light_distance = MAX_LIGHT_DISTANCE
+        inverse_max_light_distance = INVERSE_MAX_LIGHT_DISTANCE
+        screen_center = SCREEN_CENTER
+        max_flashlight_screen_distance = MAX_FLASHLIGHT_SCREEN_DISTANCE
+
+        player_pos = self.player.pos
+        player_map_pos = self.player.map_pos
         curr_ray_angle = self.player.player_angle - (FOV / 2) + 0.0001
+        max_depth = MAX_DEPTH
+        map_dic = self.game.map.map_dic
 
         for ray in range(RAY_AMT):
-            ray_info = RayCasting.cast_ray(self.player.pos, self.player.map_pos, curr_ray_angle, MAX_DEPTH, self.game.map.map_dic)
+            ray_info = RayCasting.cast_ray(player_pos, player_map_pos, curr_ray_angle, max_depth, map_dic)
 
             if ray_info[0]:
 
                 corrected_ray_depth = ray_info[1] * math.cos(self.player.player_angle - curr_ray_angle)
-                projection_height = SCREEN_DISTANCE / corrected_ray_depth
+                projection_height = screen_distance / corrected_ray_depth
+                projection_height = projection_height if projection_height <= height else height
 
-                projection_height = min(projection_height, HEIGHT)
+                distance_multiplier = 0 if ray_info[1] > max_light_distance else 1 - (ray_info[1] * inverse_max_light_distance)
 
-                darkness_multiplier = 0.7
+                projection_height_segments = projection_height / light_seg_size
+                projection_height_segments_int = int(projection_height_segments)
 
-                distance_multiplier = (1 - ray_info[1] / MAX_DEPTH)
-
-                projection_height_segments = projection_height / LIGHT_SEG_SIZE
+                ray_offset = ray * ray_width_scale
+                projection_height_offset = ((height - projection_height) // 2)
 
                 color = (255, 255, 255)
 
-                center = (WIDTH // 2, HEIGHT //2)
+                for segment in range(projection_height_segments_int):
 
-                for segment in range(int(projection_height_segments)):
+                    segment_offset = segment * light_seg_size
 
-                    location = (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + segment * LIGHT_SEG_SIZE)
+                    location = (ray_offset, (projection_height_offset + segment_offset))
+                    center_screen_distance = (location[0] - screen_center[0])**2 + (location[1] - screen_center[1])**2
+
+                    flashlight_multiplier = 1 if center_screen_distance > max_flashlight_screen_distance else Lighting.calculate_flashlight_multiplier(center_screen_distance, 1 / (distance_multiplier + 0.00001))
     
-                    color = Lighting.calculate_lighting_multiplier_flashlight((255, 255, 255), ray_info[1], location)
+                    color = [255 * (darkness_multiplier * flashlight_multiplier * distance_multiplier)]*3
 
                     pygame.draw.rect(
                         self.game.screen,
                         color,
-                        (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + segment * LIGHT_SEG_SIZE, RAY_WIDTH_SCALE, LIGHT_SEG_SIZE)
+                        (ray_offset, (projection_height_offset + segment_offset), ray_width_scale, light_seg_size)
                     )
                     
                 pygame.draw.rect(
                     self.game.screen,
                     color,
-                    (ray * RAY_WIDTH_SCALE, ((HEIGHT - projection_height) // 2) + (LIGHT_SEG_SIZE * int(projection_height_segments)), RAY_WIDTH_SCALE,  ((projection_height_segments) - int(projection_height_segments)) * LIGHT_SEG_SIZE)
+                    (ray_offset, (projection_height_offset + (light_seg_size * projection_height_segments_int)), ray_width_scale,  ((projection_height_segments) - projection_height_segments_int) * light_seg_size)
                 )
 
 
@@ -59,4 +78,4 @@ class PlayerCamera:
                 #    2
                 #    )
 
-            curr_ray_angle += RAY_DELTA_ANGLE
+            curr_ray_angle += ray_delta_angle
