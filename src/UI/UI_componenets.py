e78590fff1db044c88ed2d378c371c6fc850b288
DCritt <dcritte@clemson.edu>
2025-11-19 13:24:51 -0500

    made centering work

--- src/UI/UI_componenets.py
+++ src/UI/UI_componenets.py
@@ -3,14 +3,15 @@ from src.data.settings import *
 
 class UIComponent:
     def __init__(self, x_p, y_p, width_p, height_p):
+        self.parent = None
+        self.visible = True
+        self.active = True
+        
         self.x_p = x_p
         self.y_p = y_p
         self.width_p = width_p
         self.height_p = height_p
-        self.rect = pygame.Rect(x_p * WIDTH, y_p * HEIGHT, width_p * WIDTH, height_p * HEIGHT)
-        self.parent = None
-        self.visible = True
-        self.active = True
+        self.center_rect()
 
     def update(self):
         pass
@@ -21,33 +22,59 @@ class UIComponent:
     def draw(self, surface):
         pass
 
-    def set_parent(self, parent):
-        self.parent = parent
-        x = (self.x_p * parent.rect.x)
-        y = (self.y_p * parent.rect.y)
-        width = (self.width_p * parent.rect.width)
-        height = (self.height_p * parent.rect.height)
+    def center_rect(self):
+        parent_dim = self.get_parent_dim()
+
+        width = (self.width_p * parent_dim[0])
+        height = (self.height_p * parent_dim[1])
+
+        x = ((self.x_p * parent_dim[0]) - (width // 2))
+        x = x if x >= 0 else 0
+        x = x if x <= (parent_dim[0] - width) else (parent_dim[0] - width)
+
+        y = ((self.y_p * parent_dim[1]) - (height // 2))
+        y = y if y >= 0 else 0
+        y = y if y <= (parent_dim[1] - height) else (parent_dim[1] - height)
+
         self.rect = pygame.Rect(x, y, width, height)
 
-    def resize(self, x_p, y_p, width_p, height_p):
-        parent_dim = (WIDTH, HEIGHT) if not self.parent else (self.parent.rect.width, self.parent.rect.height)
+    def update_rect(self):
+        self.center_rect()
 
+    def set_parent(self, parent):
+        self.parent = parent
+        self.update_rect()
+
+    def resize(self, x_p, y_p, width_p, height_p):
         self.x_p = x_p
         self.y_p = y_p
         self.width_p = width_p
         self.height_p = height_p
-        self.rect = pygame.Rect((x_p * parent_dim[0]), (y_p * parent_dim[1]), (width_p * WIDTH), (height_p * HEIGHT))
+        self.update_rect()
 
     def get_relative_rect(self):
-        parent_rect = self.parent.get_relative_rect() if self.parent else (0, 0, 0, 0)
+        parent_rect = self.parent.get_relative_rect() if self.parent else pygame.Rect(0, 0, 0, 0)
         return pygame.Rect((self.rect.x + parent_rect[0]), (self.rect.y + parent_rect[1]), self.rect.width, self.rect.height)
 
+    def get_valid_font(self, parent_dim):
+        font_size = self.font_size
+
+        while font_size != 0:
+            font = pygame.font.Font(None, (int)(font_size * SCALE_SIZE))
+            width, height = font.size(self.text)
+            if (width <= parent_dim[0] and height <= parent_dim[1]):
+                return font
+            font_size -= 1
+
+    def get_parent_dim(self):
+        return (WIDTH, HEIGHT) if not self.parent else (self.parent.rect.width, self.parent.rect.height)
+
 class UIPanel(UIComponent):
-    def __init__(self, x_p, y_p, width_p, height_p, children=[]):
+    def __init__(self, x_p, y_p, width_p, height_p, children=None):
         super().__init__(x_p, y_p, width_p, height_p)
         
-        self.children = children
-        for child in children:
+        self.children = children if children is not None else []
+        for child in self.children:
             child.set_parent(self)
 
         self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
@@ -61,27 +88,38 @@ class UIPanel(UIComponent):
             child.check_event(event)
 
     def draw(self, surface):
+        self.surface.fill((0, 0, 0, 0))
         for child in self.children:
             child.draw(self.surface)
         surface.blit(self.surface, (self.rect.x, self.rect.y))
 
+    def update_rect(self):
+        super().update_rect()
+        self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+        for child in self.children:
+            child.update_rect()
+
     def set_parent(self, parent):
         super().set_parent(parent)
-        self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+        for child in self.children:
+            child.update_rect()
     
     def resize(self, x_p, y_p, width_p, height_p):
         super().resize(x_p, y_p, width_p, height_p)
-        self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
         for child in self.children:
-            child.resize(child.x_p, child.y_p, child.width_p, child.height_p)
+            child.update_rect()
 
     def add_child(self, child):
         self.children.append(child)
+        child.set_parent(self)
 
 class UILabel(UIComponent):
     def __init__(self, x_p, y_p, text, font_size, color):
+        self.parent = None
+
         self.text = text
-        self.font = pygame.font.Font(None, ((int)(font_size * SCALE_SIZE)))
+        self.font_size = font_size
+        self.font = self.get_valid_font(self.get_parent_dim())
         self.color = color
         self.surface = self.font.render(text, True, color)
 
@@ -98,21 +136,18 @@ class UILabel(UIComponent):
     def draw(self, surface):
         surface.blit(self.surface, (self.rect.x, self.rect.y))
 
-    def resize(self, x_p, y_p, width_p, height_p):
-        parent_dim = (WIDTH, HEIGHT) if not self.parent else (self.parent.rect.width, self.parent.rect.height)
-        
-        self.surface = self.font = pygame.font.Font(None, ((int)(font_size * SCALE_SIZE)))
-        width_p = self.surface.get_width() / parent_dim[0]
-        height_p = self.surface.get_height() / parent_dim[1]
+    def update_rect(self):
+        self.font = self.get_valid_font(self.get_parent_dim())
+        self.surface = self.font.render(self.text, True, self.color)
 
-        self.resize(x_p, y_p, width_p, height_p)
 
 class UIButton(UIComponent):
     def __init__(self, x_p, y_p, width_p, height_p, text, font_size, font_color, color, func):
         super().__init__(x_p, y_p, width_p, height_p)
 
         self.text = text
-        self.font = pygame.font.Font(None, ((int)(font_size * SCALE_SIZE)))
+        self.font_size = font_size
+        self.font = self.get_valid_font((self.rect.width, self.rect.height))
         self.font_color = font_color
         self.font_surface = self.font.render(text, True, font_color)
         
@@ -133,7 +168,14 @@ class UIButton(UIComponent):
 
     def draw(self, surface):
         pygame.draw.rect(surface, (self.color if not self.hovered else self.hover_color), self.rect)
-        surface.blit(self.font_surface, (self.rect.x, self.rect.y))
+        width_off = (self.rect.width - self.font_surface.get_width()) // 2
+        height_off = (self.rect.height - self.font_surface.get_height()) // 2
+        surface.blit(self.font_surface, (self.rect.x + width_off, self.rect.y + height_off))
+
+    def update_rect(self):
+        super().update_rect()
+        self.font = self.get_valid_font((self.rect.width, self.rect.height))
+        self.font_surface = self.font.render(self.text, True, self.font_color)
 
 class UIImage(UIComponent):
     def __init__(self, x_p, y_p, width_p, height_p, image_path):
--- src/data/UI_scenes.py
+++ src/data/UI_scenes.py
@@ -7,16 +7,26 @@ from src.game_management.scene import LevelScene
 def create_main_menu(game):
     pygame.init()
 
-    title_x = 0
-    title_y = 0
-    title_text = "ANALOG ABYSS"
-    title_color = (150, 0, 0)
-    title = UILabel(title_x, title_y, title_text, 72, title_color)
+    title = UILabel(
+        x_p=0,
+        y_p=0,
+        text="ANALOG ABYSS",
+        font_size=200,
+        color=(150, 0, 0)
+    )
 
     play_button_text = "Play"
-    play_button = UIButton(0, 0.1, 0.05, 0.07, play_button_text, 40, title_color, (50, 50, 50), lambda: game.load_scene(LevelScene(game, map1)))
+    play_button = UIButton(0.5, 0.35, 0.15, 0.1, play_button_text, 40, (150, 0, 0), (50, 50, 50), lambda: game.load_scene(LevelScene(game, map1)))
 
-    main_menu = UIPanel(0, 0, 1, 1, [title, play_button])
-    main_menu.resize(0, 0, 0.5, 0.5)
+    menu_panel = UIPanel(
+        x_p=0.5,
+        y_p=0.5,
+        width_p=0.4,
+        height_p=0.5,
+        children=[title, play_button]
+    )
+
+    main_menu = UIPanel(0, 0, 1, 1, [menu_panel])
+    #main_menu.resize(0, 0, 0.1, 0.1)
 
     return main_menu
